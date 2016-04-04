515d60a5e54d7e7114d753fd94e1d5795ba4a3f8
Noah Gamboa <Noah@Noah-2.local>
2016-04-04 00:50:55 -0700

    missing stuff

--- bot.js
+++ bot.js
@@ -3,6 +3,18 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/calendar-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+    var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /canonical/i;
