bd5ec4d08cd93114492c2ad580e58a11603bd17d
Noah Gamboa <Noah@Noah-2.local>
2016-04-05 14:12:54 -0700

    adding the bonz

--- data.js
+++ data.js
@@ -4,7 +4,8 @@ var botIDs = {
     // "<group id>": "<bot id>",
     "21013430":"218ad65c315e318d5c3407ac83",
     "18032921":"ff628b90a3b0a1372e326f3847",
-    "1689488":"2cfb0f7188d1d9c5392c9f3b36"
+    "1689488":"2cfb0f7188d1d9c5392c9f3b36",
+    "17134214": "ebc25e337c1c99d2026b8f85d9"
 
 }
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
