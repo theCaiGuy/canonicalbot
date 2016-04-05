dcaa48bc204d9c42e7b3d66ada2e41d07912fd56
Noah Gamboa <Noah@Noah-2.local>
2016-04-05 02:26:00 -0700

    timing bug work + clean up

--- bot.js
+++ bot.js
@@ -7,15 +7,7 @@ var botID = process.env.BOT_ID;
 var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
-
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/calendar-nodejs-quickstart.json
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-/*
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-    var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
-    */
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
@@ -58,14 +50,15 @@ function generateMessage(auth, botResponse, options, body, botReq) {
             botResponse = 'No upcoming events found.';
             console.log('No upcoming events found.');
         } else {
-            console.log('Upcoming in the next seven days:');
+            console.log("events sent:");
             botResponse = "~~" + quote() + "~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                 for (var i = 0; i < events.length; i++) {
                     var event = events[i];
                     var start = event.start.dateTime || event.start.date;
                     start = new Date(start);
-                    botResponse += dateFormat(start, "ddd @ h:MMTT") +  ": " + event.summary + "\n";
-                    console.log('%s - %s', start, event.summary);
+                    var event = dateFormat(start, "ddd @ h:MMTT") +  ": " + event.summary + "\n";
+                    botResponse += event
+                    console.log(event);
                 }
             botResponse += "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         }
@@ -81,7 +74,7 @@ function generateMessage(auth, botResponse, options, body, botReq) {
             "text" : botResponse
         };
 
-        console.log('sending ' + botResponse + ' to ' + botID);
+        //console.log('sending ' + botResponse + ' to ' + botID);
 
         botReq = HTTPS.request(options, function(res) {
             if(res.statusCode == 202) {
@@ -123,197 +116,7 @@ function postMessage() {
       generateMessage(jwtClient, botResponse, options, body, botReq) 
           // Make an authorized request to list Drive files.
   });
-  /* TRY TWO - FAILED
-  request({
-      //url: 'https://www.googleapis.com/calendar/v3',
-      url: 'https://www.googleapis.com/auth/calendar.readonly',
-      jwt: {
-          //use the email address of the service account, as seen in the API console
-          email: 'gmc-212@groupmecanonical.iam.gserviceaccount.com',
-          // use the PEM file we generated from the downloaded key
-          keyFile: 'groupMeCanonicalKey.pem',
-          // specify the scopes you wish to access
-          scopes: SCOPES
-      }
-  }, function (err, res, body) {
-      if (err) {
-          console.log("authentication returned an error", err);
-          return
-      }
-      console.log(body);
-      console.log(res.result);
-      for (var property in res) {
-          console.log("HERES ONE", property);
-      }
-      //console.log(JSON.parse(body));
-      //generateMessage(auth, botResponse, options, body, botReq) 
-  });
-  */
-  /* TRY ONE - WORKED SORTA
-  fs.readFile('groupMeCanonical-8e8bda5bb3c9.json', function processClientSecrets(err, content) {
-      if (err) {
-          console.log('Error loading client secret file: ' + err);
-          return;
-      }
-      // Authorize a client with the loaded credentials, then call the
-      // Google Calendar API.
-      authorize(JSON.parse(content), function(auth) {
-        generateMessage(auth, botResponse, options, body, botReq) 
-      });
-  });
-  */
-
 }
 
-/*
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-//function authorize(credentials, callback) {
-    /*
-    var clientSecret = credentials.installed.client_secret;
-    var clientId = credentials.installed.client_id;
-    var redirectUrl = credentials.installed.redirect_uris[0];
-    var auth = new googleAuth();
-    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-    */
-    /*
-    iat = new Date();
-    //var jwtstr = '{"alg":"RS256","typ":"JWT"}.  { "iss":"' + credentials.client_email + '", "scope":"' + SCOPES[0] + '", "aud":"https://www.googleapis.com/oauth2/v4/token", "exp":' + 
-    //   ((iat.getTime() / 1000)+(60*60)) + '", iat":' + (iat.getTime() / 1000) + '}.'
-    var payload =  {
-        "iss": credentials.client_email,
-        "scope": SCOPES[0],
-        "aud":"https://www.googleapis.com/oauth2/v4/token",
-        "sub":"webmaster@lsjumb.com",
-        "exp":((iat.getTime() / 1000)+(60*60)),
-        "iat":(iat.getTime() / 1000) 
-    }
-    var headers = {
-        "alg":"RS256",
-        "typ":"JWT"
-    }
-
-    var token = jwt.sign(payload, credentials.private_key, {algorithm: 'RS256', headers: headers});
-    console.log(token);
-    var decoded = jwt.verify(token, credentials.private_key, { algorithms: ['RS256'] });
-    console.log(decoded);
-    */
-/*
-    googleAuth.authenticate({
-        //use the email address of the service account, as seen in the API console
-            email: 'gmc-212@groupmecanonical.iam.gserviceaccount.com',
-        // use the PEM file we generated from the downloaded key
-            keyFile: 'groupMeCanonicalKey.pem',
-            // specify the scopes you wish to access
-            scopes: SCOPES
-    }, function (err, token) {
-        console.log(token);
-    });
-    */
-    /*
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, function(err, token) {
-        if (err) {
-            getNewToken(oauth2Client, callback);
-        } else {
-     //       oauth2Client.credentials = JSON.parse(token);
-            callback(oauth2Client);
-        }
-    });
-    */
-//}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-/*
-function getNewToken(oauth2Client, callback) {
-    var authUrl = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES
-    });
-    console.log(authUrl);
-    var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.question('Enter the code from that page here: ', function(code) {
-        console.log("in callback");
-        rl.close();
-        oauth2Client.getToken(code, function(err, token) {
-            if (err) {
-                console.log('Error while trying to retrieve access token', err);
-                return;
-            }
-            oauth2Client.credentials = token;
-            storeToken(token);
-            callback(oauth2Client);
-        });
-    });
-}
-*/
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-/*
-function storeToken(token) {
-    try {
-        fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-        if (err.code != 'EEXIST') {
-            throw err;
-        }
-    }
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    console.log('Token stored to ' + TOKEN_PATH);
-}
-*/
-
-/**
- * Lists the next 10 events on the user's primary calendar.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-/*
-function listEvents(auth) {
-    var calendar = google.calendar('v3');
-    calendar.events.list({
-        auth: auth,
-        calendarId: 'primary',
-        timeMin: (new Date()).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime'
-    }, function(err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-        }
-        var events = response.items;
-        if (events.length == 0) {
-            console.log('No upcoming events found.');
-        } else {
-            console.log('Upcoming 10 events:');
-            for (var i = 0; i < events.length; i++) {
-                var event = events[i];
-                var start = event.start.dateTime || event.start.date;
-                console.log('%s - %s', start, event.summary);
-            }
-        }
-    });
-}
-*/
 
 exports.respond = respond;
