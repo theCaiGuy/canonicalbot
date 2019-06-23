f61cc3ce30fe085be2174fdb8dcc3e1256b9e25e
theCaiGuy <michaelcai09@gmail.com>
2019-06-23 01:26:23 -0700

    Updated group IDs

--- data.js
+++ data.js
@@ -2,13 +2,11 @@ var botIDs = {
     // group id (from request) : bot id (to send back to right group)
     // like this:
     // "<group id>": "<bot id>",
-    "21013430":"218ad65c315e318d5c3407ac83",
-    "18032921":"ff628b90a3b0a1372e326f3847",
-    "1689488":"2cfb0f7188d1d9c5392c9f3b36",
-    "17134214": "ebc25e337c1c99d2026b8f85d9"
+    "1689488":"ce60c19434e25efea5e286ed9e",
+    "37009291": "75833ac167c8ccfd5fbc69f909"
 
 }
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 var calendarId = 'lshal8co1phedgq1dl77h2fkhs@group.calendar.google.com'
-var serviceAccountEmail = 'gmc-658@groupmecanonical.iam.gserviceaccount.com' 
+var serviceAccountEmail = 'gmc-658@groupmecanonical.iam.gserviceaccount.com'
 var impersonatedAccount = 'webmaster@lsjumb.com'
